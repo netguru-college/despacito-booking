31b49e0c508f3529f79d49c0c16a47b2e9d4ad71
Dejan Acimovic <dejanacim@gmail.com>
2018-05-26 15:40:11 +0200

    Add beta version of pdf receipt generator

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,28 @@
 class Payment < ApplicationRecord
   belongs_to :booking
+
+  def receipt
+    Receipts::Receipt.new(
+      id: id,
+      product: "Resource Name",
+      company: {
+        name: "DespacitoBooking, Inc.",
+        address: "Al. Grunwaldzka 103A\n80-244 Gdańsk\nPoland",
+        email: "hello@netguru.co",
+        logo: Rails.root.join("app/assets/images/netguru-logo.png")
+      },
+      line_items: [
+        ["Date",           created_at.to_s],
+        ["Account Billed", "#{user.name} (#{user.email})"],
+        ["Product",        "Resource Name"],
+        ["Amount",         "$#{amount / 100}.00"],
+        ["Charged to",     "#{card_type} (**** **** **** #{card_last4})"],
+        ["Transaction ID", uuid]
+      ],
+      font: {
+        bold: Rails.root.join('app/assets/fonts/tradegothic/TradeGothic-Bold.ttf'),
+        normal: Rails.root.join('app/assets/fonts/tradegothic/TradeGothic.ttf'),
+      }
+    )
+  end
 end
