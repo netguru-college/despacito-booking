5b8cde050b6febae8b57b6c5323d1b7ccebbbb54
ksarzynski <kamil.s.sarzynski@gmail.com>
2018-05-26 12:35:43 +0200

    remove payment_id from booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,8 @@
 class Booking < ApplicationRecord
  # belongs_to :user
  # belongs_to :resource
- # belongs_to :payment
  validates :user_id, presence: true
  validates :resource_id, presence: true
- validates :payment_id, presence: true
  validates :date_from, presence: true
  validates :date_to, presence: true
  validates :total_price, numericality: { greater_than_or_equal_to: 0}, presence: true
