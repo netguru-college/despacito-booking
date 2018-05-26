1b184c7b5efec736c8006806dd57ca3c2c34500c
Malwina Tarnowska <sobmal@gmail.com>
2018-05-26 16:42:44 +0200

    Payment contoller has been added

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,34 @@
 class PaymentsController < ApplicationController
-  
+  before_action :find_booking
+  skip_before_action :authenticate_user!
 
+
+  def create
+    @payment = Payment.new(booking_id: params[:booking_id], status: "unpaid")
+
+    if @payment.save
+      redirect_to @booking, notice: "Payment has been succesfully added!"
+    else
+      redirect_to @booking, alert: "Payment failed!"
+    end
+  end
+
+  def update
+    @payment = Payment.find(params[:id])
+
+    if @payment.update(payment_params)
+      redirect_to @booking, notice: "The Payment has been updated"
+    else
+      redirect_to @booking, alert: "The Payment update has failed"
+    end
+  end
+
+  private
+  def find_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:status)
+  end
 end
