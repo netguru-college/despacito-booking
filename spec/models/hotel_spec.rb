782691cfa31aa24d708c540a8abd5d2c2def4158
Dejan Acimovic <dejanacim@gmail.com>
2018-05-26 12:25:39 +0200

    Add basic hotel model specs

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Hotel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.name = "Anything"
+    subject.address = "Somewhere"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without an address" do
+    expect(subject).to_not be_valid
+  end
 end
