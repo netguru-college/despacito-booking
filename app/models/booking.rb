58610205ebe85a30ba195c7cab43fbbb23e5fc89
ksarzynski <kamil.s.sarzynski@gmail.com>
2018-05-26 16:47:01 +0200

    indentation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,12 @@
 class Booking < ApplicationRecord
- belongs_to :user
- belongs_to :resource
- validates :user_id, presence: true
- validates :resource_id, presence: true
- validates :date_from, presence: true
- validates :date_to, presence: true
- validates :total_price, numericality: { greater_than_or_equal_to: 0}, presence: true
- validate  :correct_date?
+  belongs_to :user
+  belongs_to :resource
+  validates :user_id, presence: true
+  validates :resource_id, presence: true
+  validates :date_from, presence: true
+  validates :date_to, presence: true
+  validates :total_price, numericality: { greater_than_or_equal_to: 0}, presence: true
+  validate  :correct_date?
 
  def correct_date?
    if self.date_from < DateTime.now
