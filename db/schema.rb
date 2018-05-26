ec66495b54cfb5f15c10429b2afbc17657639cfd
Dejan Acimovic <dejanacim@gmail.com>
2018-05-26 13:03:44 +0200

    Add email and phone columns to hotel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_100844) do
+ActiveRecord::Schema.define(version: 2018_05_26_110015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,6 @@ ActiveRecord::Schema.define(version: 2018_05_26_100844) do
     t.datetime "date_from"
     t.datetime "date_to"
     t.decimal "total_price"
-
-  create_table "resources", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,6 +29,15 @@ ActiveRecord::Schema.define(version: 2018_05_26_100844) do
   create_table "hotels", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "phone"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
