9c6b4509371450570ab8d62ead060b8c6943caa3
Dejan Acimovic <dejanacim@gmail.com>
2018-05-26 12:13:12 +0200

    Add hotel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_091804) do
+ActiveRecord::Schema.define(version: 2018_05_26_100844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2018_05_26_091804) do
     t.datetime "date_from"
     t.datetime "date_to"
     t.decimal "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
