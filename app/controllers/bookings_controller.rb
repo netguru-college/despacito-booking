48dfc62ad79dae34cc301d09bf68866369fc4da7
ksarzynski <kamil.s.sarzynski@gmail.com>
2018-05-27 12:45:50 +0200

    update controller add pay button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,22 @@
 class BookingsController < ApplicationController
-  
+
   def index
     @bookings = Booking.joins(:resource).where(:bookings => {:user_id => current_user})
   end
+
+  def show
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: "Booking has been succesfully added!"
+    else
+      redirect_to @booking, alert: "Booking failed!"
+    end
+end
+
+def booking_params
+  params.require(:booking).permit(:date_from, :date_to, :total_price)
+end
 end
