09b32f88821faebbf6295959f8082e2380261203
Arkadiusz Bochenek <arkadiusz.bochenek@gmail.com>
2018-05-27 15:09:45 +0200

    Fix rspec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     # f.surname { Faker::Name.last_name }
     f.email { Faker::Internet.unique.free_email }
     f.password { Faker::Internet.password(8) }
+
+    trait :admin do
+      role 'admin'
+    end
+
+    factory :admin, traits: [:admin]
   end
 end
